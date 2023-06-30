7ac9c1727a9d208cd13cd46ac6fa2c040fb7f3f6
anthonygavriel <anthonygavriel@gmail.com>
2023-06-30 11:30:41 +0100

    Git Test

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ int main() {
     int deckSize = deck.getSize();
     std::cout << deckSize << std::endl;
     deck.printDeck();
+    std::cout << "\n";
+
+    if(deck.getSize() == 52) {
+        std::cout << "Full deck instantiated.";
+    }
 
     return 0;
 }
