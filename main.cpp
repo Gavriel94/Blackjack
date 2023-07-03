29e168e087f7ea05126423b3cf1d00eed1e2644b
anthonygavriel <anthonygavriel@gmail.com>
2023-07-03 20:47:45 +0100

    Program branches added

--- main.cpp
+++ main.cpp
@@ -1,18 +1,46 @@
-#include <iostream>
-
 #include "include/Card.h"
 #include "include/Deck.h"
+#include "include/Player.h"
+#include "include/Dealer.h"
+#include "Tests/UnitTests.cpp"
 
-int main() {
-    Deck deck;
-    int deckSize = deck.getSize();
-    std::cout << deckSize << std::endl;
-    deck.printDeck();
-    std::cout << "\n";
+void runTests(const std::string& testClass) {
+    UnitTest unitTest = UnitTest();
 
-    if(deck.getSize() == 52) {
-        std::cout << "Full deck instantiated.";
+    if(testClass == "Card") {
+        unitTest.cardTests();
+    } else if(testClass == "Dealer") {
+        unitTest.dealerTests();
+    } else if(testClass == "Deck") {
+        unitTest.deckTests();
+    } else if(testClass == "Player") {
+        unitTest.playerTests();
+    } else if(testClass == "All") {
+        unitTest.allTests();
+    } else {
+        std::cout << "\n\nInvalid Argument " << testClass << "\n";
+        std::cout << "Options for tests are:\n";
+        std::cout << "- \"Card\"\n";
+        std::cout << "- \"Dealer\"\n";
+        std::cout << "- \"Deck\"\n";
+        std::cout << "- \"Player\"\n\n";
+        std::cout << "Program terminating. Please try again.";
+    }
+}
+
+void runGame() {
+
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        runTests(std::string(argv[1]));
+    }
+    else {
+        runGame();
     }
 
     return 0;
 }
+
+
