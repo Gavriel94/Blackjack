68d05488e37c0d2ef823468c0a3ac4953060a766
anthonygavriel <anthonygavriel@gmail.com>
2023-07-03 20:46:13 +0100

    Host for tests

---
+++ Tests/UnitTests.cpp
@@ -0,0 +1,99 @@
+//
+// Created by Anthony Gavriel on 30/06/2023.
+//
+
+#include <functional>
+#include <iostream>
+
+#include "CardTest.cpp"
+#include "DeckTest.cpp"
+#include "PlayerTest.cpp"
+#include "DealerTest.cpp"
+
+class UnitTest {
+public:
+    UnitTest() = default;
+
+    void callClassTest(const std::function<void()>& test, const std::string& testName) {
+        test();
+        functionFooter(testName);
+    }
+
+    void cardTests() {
+        std::string className = "Card";
+        classTestHeader(className);
+        callClassTest([&]() { CardTest::testGetSuit(); }, "getSuit()");
+        callClassTest([&]() { CardTest::testGetValue(); }, "getValue()");
+        classTestFooter(className);
+        newLine();
+    }
+
+    void deckTests() {
+        std::string className = "Deck";
+        classTestHeader(className);
+        callClassTest([&]() { DeckTest::testCreate(); }, "createDeck()");
+        callClassTest([&]() { DeckTest::testShuffle(); }, "shuffleDeck()");
+        callClassTest([&]() { DeckTest::testRemoveCard(); }, "removeCard()");
+        classTestFooter(className);
+        newLine();
+    }
+
+    void playerTests() {
+        std::string  className = "Player";
+        classTestHeader(className);
+
+        callClassTest([&]() { PlayerTest::testGetName(); }, "getName()");
+        callClassTest([&]() { PlayerTest::testAce(); }, "ace()");
+        callClassTest([&]() { PlayerTest::testReceiveCard(); }, "receiveCard()");
+        callClassTest([&]() { PlayerTest::testIsBust(); }, "isBust()");
+
+        classTestFooter(className);
+        newLine();
+    }
+
+    void dealerTests() {
+        std::string className = "Dealer";
+        classTestHeader(className);
+
+        callClassTest([&]() { DealerTest::testStartGame(); }, "startGame()");
+        callClassTest([&]() { DealerTest::testReceiveCard(); }, "receiveCard()");
+        callClassTest([&]() { DealerTest::testHandValue(); }, "getHandValue()");
+        callClassTest([&]() { DealerTest::testBlackjack(); }, "blackjack()");
+        callClassTest([&]() { DealerTest::testBust(); }, "testBust()");
+        callClassTest([&]() { DealerTest::testStick(); }, "testStick()");
+
+        classTestFooter(className);
+        newLine();
+    }
+
+    void allTests() {
+        cardTests();
+        deckTests();
+        playerTests();
+        dealerTests();
+    }
+
+    void classTestHeader(const std::string& testedClass) {
+        line();
+        std::cout << "    Unit tests for " << testedClass << " class   \n";
+        line();
+    }
+
+    void classTestFooter(const std::string& testedClass) {
+        std::cout << "        " << testedClass << " tests Completed      \n";
+        line();
+    }
+
+    void functionFooter(const std::string& functionName) {
+        std::cout << "        " << functionName << " passed   \n";
+        line();
+    }
+
+    static void line() {
+        std::cout << "*----------------------------------*\n";
+    }
+
+    static void newLine() {
+        std::cout << "\n";
+    }
+};
