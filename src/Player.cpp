58ea9c65ba8d8d6d8c45f4a1644fe80e5a4a55f6
anthonygavriel <anthonygavriel@gmail.com>
2023-07-03 20:46:45 +0100

    functionality added

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,58 +3,87 @@
 //
 
 #include "../include/Player.h"
-#include "../include/Card.h"
-#include <utility>
 
 Player::Player(std::string name) {
     this -> name = std::move(name);
     handValue = 0;
     playing = true;
+    bust = false;
+    blackjack = false;
+}
+
+void Player::receiveCard(Card card) {
+    int value = 0;
+    if(card.getValue() == "Ace") {
+        value = ace();
+    } else {
+        value = stoi(card.getValue());
+    }
+    hand.push_back(card);
+    handValue += value;
+    if(handValue > 21) {
+        bust = true;
+        playing = false;
+    }
+    if(handValue == 21 && hand.size() == 2) {
+        std::cout << "Blackjack!";
+        playing = false;
+        blackjack = true;
+    }
 }
 
 int Player::ace() {
-    std::string choice;
+    int choice;
     std::cout << "Would you like the Ace to be a 1 or 11?\n";
     std::cout << "Type '1' or '11' and press enter.\n";
     std::cin >> choice;
-    if(choice == "1") {
-        return 1;
-    } else if(choice == "11") {
-        return 11;
-    } else {
-        ace();
+    switch(choice) {
+        case 1:
+            return 1;
+        case 11:
+            return 11;
+        default:
+            std::cout << "Please select a valid option: (1/11).\n";
+            std::cin >> choice;
     }
+    return 1;
 }
 
 int Player::getHandValue() {
-    int currentValue = 0;
-    for(auto card : hand) {
-        if(
-                card.getValue() == "JACK" ||
-                card.getValue() == "QUEEN" ||
-                card.getValue() == "KING"
-                ) {
-            currentValue += 10;
-        } else if(card.getValue() == "ACE") {
-            currentValue += ace();
-        } else {
-            currentValue += std::stoi(card.getValue());
-        }
-    }
-    handValue = currentValue;
-    return currentValue;
+    return handValue;
 }
 
-std::string Player::viewCards() {
-    std::cout << name + "'s cards:\n";
-    for(auto card : hand) {
-        std::cout << card;
-        std::cout << "\n";
-    }
-    std::cout << "\n";
+std::vector<Card> Player::getHand() {
+    return hand;
 }
 
+bool Player::hit() {
+    return true;
+}
 
+void Player::stick() {
+    playing = false;
+}
 
+bool Player::isPlaying() {
+    return playing;
+}
 
+bool Player::isBust() {
+    return bust;
+}
 
+std::string Player::getName() {
+    return name;
+}
+
+bool Player::getBlackjack() {
+    return blackjack;
+}
+
+void Player::printHand() {
+    std::cout << name << "'s cards\n";
+    for(auto card: hand) {
+        std::cout << card.getValue() << " of " << card.getSuit() << "\n";
+    }
+}
