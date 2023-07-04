8896f08879980da0099461911f24f868db8ddd04
anthonygavriel <anthonygavriel@gmail.com>
2023-07-04 10:52:39 +0100

    make

---
+++ build/CMakeFiles/Blackjack.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/anthonygavriel/Local/Blackjack/Tests/CardTest.cpp" "CMakeFiles/Blackjack.dir/Tests/CardTest.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/Tests/CardTest.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/Tests/DealerTest.cpp" "CMakeFiles/Blackjack.dir/Tests/DealerTest.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/Tests/DealerTest.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/Tests/DeckTest.cpp" "CMakeFiles/Blackjack.dir/Tests/DeckTest.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/Tests/DeckTest.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/Tests/GameComponentsTest.cpp" "CMakeFiles/Blackjack.dir/Tests/GameComponentsTest.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/Tests/GameComponentsTest.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/Tests/PlayerTest.cpp" "CMakeFiles/Blackjack.dir/Tests/PlayerTest.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/Tests/PlayerTest.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/Tests/UnitTests.cpp" "CMakeFiles/Blackjack.dir/Tests/UnitTests.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/Tests/UnitTests.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/main.cpp" "CMakeFiles/Blackjack.dir/main.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/main.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/src/Card.cpp" "CMakeFiles/Blackjack.dir/src/Card.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/src/Card.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/src/Dealer.cpp" "CMakeFiles/Blackjack.dir/src/Dealer.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/src/Dealer.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/src/Deck.cpp" "CMakeFiles/Blackjack.dir/src/Deck.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/src/Deck.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/src/GameComponents.cpp" "CMakeFiles/Blackjack.dir/src/GameComponents.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/src/GameComponents.cpp.o.d"
+  "/Users/anthonygavriel/Local/Blackjack/src/Player.cpp" "CMakeFiles/Blackjack.dir/src/Player.cpp.o" "gcc" "CMakeFiles/Blackjack.dir/src/Player.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "Blackjack"
+  "Blackjack.pdb"
+  "CMakeFiles/Blackjack.dir/Tests/CardTest.cpp.o"
+  "CMakeFiles/Blackjack.dir/Tests/CardTest.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/Tests/DealerTest.cpp.o"
+  "CMakeFiles/Blackjack.dir/Tests/DealerTest.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/Tests/DeckTest.cpp.o"
+  "CMakeFiles/Blackjack.dir/Tests/DeckTest.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/Tests/GameComponentsTest.cpp.o"
+  "CMakeFiles/Blackjack.dir/Tests/GameComponentsTest.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/Tests/PlayerTest.cpp.o"
+  "CMakeFiles/Blackjack.dir/Tests/PlayerTest.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/Tests/UnitTests.cpp.o"
+  "CMakeFiles/Blackjack.dir/Tests/UnitTests.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/main.cpp.o"
+  "CMakeFiles/Blackjack.dir/main.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/src/Card.cpp.o"
+  "CMakeFiles/Blackjack.dir/src/Card.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/src/Dealer.cpp.o"
+  "CMakeFiles/Blackjack.dir/src/Dealer.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/src/Deck.cpp.o"
+  "CMakeFiles/Blackjack.dir/src/Deck.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/src/GameComponents.cpp.o"
+  "CMakeFiles/Blackjack.dir/src/GameComponents.cpp.o.d"
+  "CMakeFiles/Blackjack.dir/src/Player.cpp.o"
+  "CMakeFiles/Blackjack.dir/src/Player.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
