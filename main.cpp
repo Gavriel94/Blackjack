9b6bfc6963d4702e52e515746df4e70dde840610
anthonygavriel <anthonygavriel@gmail.com>
2023-07-06 14:44:44 +0100

    reformatted

--- main.cpp
+++ main.cpp
@@ -1,115 +1,14 @@
-#include "include/Card.h"
-#include "include/Deck.h"
-#include "include/Player.h"
-#include "include/Dealer.h"
-#include "Tests/UnitTests.cpp"
-#include "include/GameComponents.h"
-
-void runTests(const std::string& testClass) {
-    UnitTest unitTest = UnitTest();
-
-    if(testClass == "Card") {
-        unitTest.cardTests();
-    } else if(testClass == "Dealer") {
-        unitTest.dealerTests();
-    } else if(testClass == "Deck") {
-        unitTest.deckTests();
-    } else if(testClass == "Player") {
-        unitTest.playerTests();
-    } else if(testClass == "GameComponents") {
-      unitTest.gameComponentsTests();
-    } else if(testClass == "All") {
-        unitTest.allTests();
-    } else {
-        std::cout << "\n\nInvalid Argument \"" << testClass << "\"\n\n";
-        std::cout << "Options for tests are:\n";
-        std::cout << "- \"Card\"\n";
-        std::cout << "- \"Dealer\"\n";
-        std::cout << "- \"Deck\"\n";
-        std::cout << "- \"Player\"\n";
-        std::cout << "\"GameComponents\"\n\n";
-        std::cout << "Program terminating. Please try again.";
-    }
-}
-
-/**
- * Increases readability
- */
-void lineBreak() {
-    std::cout << "\n";
-}
-
-void runGame() {
-    GameComponents game = GameComponents();
-    Deck deck = game.setupDeck();
-    Dealer dealer = game.setupDealer();
-
-    game.gameIntro();
-    lineBreak();
-
-    std::vector<Player> players = game.setupPlayers();
-    std::cout << "Seated at the table we have:\n\n";
-    for(auto player : players) {
-        std::cout << "- " << player.getName() << "\n";
-    }
-    lineBreak();
-
-    dealer.startGame(deck, players);
-
-
-    dealer.printHand();
-    lineBreak();
-
-    for(auto& player: players) {
-        while(player.getPlaying()) {
-            player.printHand();
-            bool hit = player.hitOrStick();
-            if(hit) {
-                Card card = deck.removeCard();
-                player.receiveCard(card);
-                if(player.isBust()) {
-                    std::cout << game.getBustGraphic(player.getName(), player.getHandValue());
-                }
-                if(player.getBlackjack()) {
-                    std::cout << game.getBlackjackGraphic();
-                    break;
-                }
-            } else {
-                player.stick();
-            }
-        }
-    }
-
-    while(dealer.getPlaying()) {
-        Card card = deck.removeCard();
-        dealer.receiveCard(card);
-        dealer.printHand();
-        if(dealer.getBust()) {
-            std::cout << game.getBustGraphic("Dealer", dealer.getHandValue());
-        }
-        if(dealer.getBlackjack()) {
-            std::cout << game.getBlackjackGraphic();
-            break;
-        }
-    }
-
-    // all hands played, now tally up scores
-
-    for(auto& player: players) {
-        std::cout << player.getName() << " has " << player.getHandValue() << "\n";
-    }
-    std::cout << "Dealer has " << dealer.getHandValue() << "\n";
-}
+#include "include/TestingSuite.h"
+#include "include/RunGame.h"
 
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        runTests(std::string(argv[1]));
+    if (argc > 1 && std::string(argv[1]) == "test") {
+        TestingSuite testingSuite = TestingSuite();
+        testingSuite.testHost();
     }
     else {
-        runGame();
+        RunGame game = RunGame();
+        game.initialiseGame();
     }
-
     return 0;
 }
-
-
