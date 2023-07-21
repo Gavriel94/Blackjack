e6836d959d42311d9d69e553d789911e69cc2f18
anthonygavriel <anthonygavriel@gmail.com>
2023-07-21 09:43:35 +0100

    Documentation added

--- include/Tests/GameComponentsTest.h
+++ include/Tests/GameComponentsTest.h
@@ -1,21 +1,65 @@
-//
-// Created by Anthony Gavriel on 06/07/2023.
-//
+/**
+ * @file GameComponentsTest.h
+ * @brief The declaration of the GameComponentsTest class and its functions.
+ * @author Anthony Gavriel
+ * @date 06/07/2023
+ */
 
 #ifndef BLACKJACK_GAMECOMPONENTSTEST_H
 #define BLACKJACK_GAMECOMPONENTSTEST_H
 
 #include "../GameComponents.h"
 
+/**
+ * @class GameComponentsTest
+ * @brief Used to test the functionality of the GameComponents class.
+ */
 class GameComponentsTest {
 public:
+    /**
+     * @brief Tests the setupPlayer() function defined in GameComponents.h.
+     *
+     * Tests the setupPlayer() function with 1 game player.
+     * As this function requires user input, the tester is displayed the conditions required to pass the test.
+     */
     static void setupOnePlayer();
+
+    /**
+     * @brief Tests the setupPlayer() function defined in GameComponents.h.
+     *
+     * Tests the setupPlayer() function with 1 game player.
+     * As this function requires user input, the tester is displayed the conditions required to pass the test.
+     */
     static void setupThreePlayers();
+
+    /**
+     * @brief Tests the setupDeck() function defined in GameComponents.h.
+     *
+     * Compares a deck created manually and through the setupDeck() function.
+     * Asserts the decks are the same before the GameComponents deck is shuffled, then another assertion ensures they are different.
+     */
     static void setupDeck();
+
+    /**
+     * @brief Tests the setupDealer() function defined in GameComponents.h.
+     *
+     * Creates a new dealer object and ensures it is playing.
+     */
     static void setupDealer();
+
+    /**
+     * @brief Tests the printHandHelper() function defined in GameComponents.h.
+     *
+     * Visual test, the user must ensure the cards are displayed correctly in the terminal.
+     */
     static void printHandHelper();
 private:
-    GameComponentsTest(); // prevents instance of class
+    /**
+     * @brief Default constructor for GameComponentsTest.
+     *
+     * The constructor is private so the class is not instantiated.
+     */
+    GameComponentsTest();
 };
 
 #endif //BLACKJACK_GAMECOMPONENTSTEST_H
--- src/Tests/GameComponentsTest.cpp
+++ src/Tests/GameComponentsTest.cpp
@@ -1,17 +1,29 @@
-//
-// Created by Anthony Gavriel on 06/07/2023.
-//
+/**
+ * @file GameComponentsTest.cpp
+ * @brief The implementation of the GameComponentsTest class and its functions.
+ * @author Anthony Gavriel
+ * @date 06/07/2023
+ */
 
 #include "../../include/Tests/GameComponentsTest.h"
 
+/**
+ * @brief Default constructor for GameComponentsTest.
+ */
 GameComponentsTest::GameComponentsTest() = default;
 
+/**
+ * @brief See declaration in GameComponentsTest.h for details.
+ */
 void GameComponentsTest::setupOnePlayer() {
     std::vector<Player> players = GameComponents::setupPlayers();
 
     assert(players.at(0).getName() == "Bob");
 }
 
+/**
+ * @brief See declaration in GameComponentsTest.h for details.
+ */
 void GameComponentsTest::setupThreePlayers() {
     std::vector<Player> players = GameComponents::setupPlayers();
 
@@ -20,6 +32,9 @@ void GameComponentsTest::setupThreePlayers() {
     assert(players.at(2).getName() == "Terry");
 }
 
+/**
+ * @brief See declaration in GameComponentsTest.h for details.
+ */
 void GameComponentsTest::setupDeck() {
     Deck deckStatic = GameComponents::setupDeck();
     Deck deckInstance = Deck();
@@ -31,12 +46,18 @@ void GameComponentsTest::setupDeck() {
     assert(deckStatic.getDeck() != deckInstance.getDeck());
 }
 
+/**
+ * @brief See declaration in GameComponentsTest.h for details.
+ */
 void GameComponentsTest::setupDealer() {
     Dealer dealer = GameComponents::setupDealer();
 
     assert(dealer.getPlaying() == true);
 }
 
+/**
+ * @brief See declaration in GameComponentsTest.h for details.
+ */
 void GameComponentsTest::printHandHelper() {
     Card ace = Card(Card::SPADES, Card::ACE);
     ace.setSpadeAscii(14);
@@ -47,12 +68,23 @@ void GameComponentsTest::printHandHelper() {
     cardVector.push_back(ace);
     cardVector.push_back(king);
 
+
+    /**
+     * For each card in their hand:
+     * - Use printHandHelper() to get a line of the card.
+     * - Append each line to cardLines.
+     */
     std::vector<std::vector<std::string>> cardLines;
     cardLines.reserve(cardVector.size());
     for (const auto &card: cardVector) {
         cardLines.push_back(GameComponents::printHandHelper(card.getAscii(), '\n'));
     }
 
+    /**
+     * - For each line, iterate over ASCII representations in cardLines.
+     * - Print the i-th line of the card.
+     * - Continue until all lines are completed, resulting in side-by-side cards.
+     */
     for(size_t i = 0; i < cardLines[0].size(); ++i) {
         for(const auto& card : cardLines) {
             std::cout << card[i] << " ";
